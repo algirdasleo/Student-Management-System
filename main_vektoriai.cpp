beec5cd652d9ace9aeda760121a1e261642b88ee
Plexukas <algirdasleonardas@gmail.com>
2024-02-10 15:55:17 +0200

    pridetas meniu, iskeltos kelios funkcijos

--- main_vektoriai.cpp
+++ main_vektoriai.cpp
@@ -12,64 +12,26 @@ struct studentaiStruct {
     vector<int> nd;
     double ndSuma, egzas, galutinisVid, mediana, galutinisMed;
 };
-vector<studentaiStruct> stud;
 
-int main() {
-    srand(time(nullptr));
-    int maxVardoIlgis = 6, maxPavardesIlgis = 7; 
+void skaiciavimai (studentaiStruct &studentas) {
+    int ndKiekis = studentas.nd.size();
+    sort(studentas.nd.begin(), studentas.nd.end());
+    if (ndKiekis % 2 == 0)
+        studentas.mediana = (studentas.nd[ndKiekis / 2] + studentas.nd[(ndKiekis / 2) - 1]) / 2.0;
+    else
+        studentas.mediana = studentas.nd[ndKiekis / 2];
+    studentas.galutinisVid = 0.4 * (studentas.ndSuma / ndKiekis) + 0.6 * studentas.egzas;
+    studentas.galutinisMed = 0.4 * studentas.mediana + 0.6 * studentas.egzas;
+}
 
-    while (true) {
-        studentaiStruct studentas;
-        studentas.ndSuma = 0;
+void vardoIvedimas (studentaiStruct &studentas) {
+    cout << "Iveskite studento varda:" << endl;
+    cin >> studentas.vardas;
+    cout << "Iveskite studento pavarde:" << endl;
+    cin >> studentas.pavarde;
+} 
 
-        cout << "Iveskite savo varda:" << endl;
-        cin >> studentas.vardas;
-        cout << "Iveskite savo pavarde:" << endl;
-        cin >> studentas.pavarde;
-
-        int vardoIlgis = studentas.vardas.length();
-        int pavardesIlgis = studentas.pavarde.length();
-        maxVardoIlgis = max(maxVardoIlgis, vardoIlgis);
-        maxPavardesIlgis = max(maxPavardesIlgis, pavardesIlgis);
-
-        char pasirinkimas;
-        int i = 0;
-        while (true) {
-            int ndRezultatas;
-            cout << "Iveskite " << i + 1 << " namu darbo rezultata:" << endl;
-            cin >> ndRezultatas;
-            studentas.nd.push_back(ndRezultatas);
-            studentas.ndSuma += ndRezultatas;
-            cout << "Ar norite ivesti dar viena namu darbo rezultata? (y/n)" << endl;
-            cin >> pasirinkimas;
-            pasirinkimas = tolower(pasirinkimas);
-            if (pasirinkimas == 'n')
-                break;
-            i++;
-        }
-        sort(studentas.nd.begin(), studentas.nd.end());
-
-        int ndKiekis = studentas.nd.size();
-        if (ndKiekis % 2 == 0)
-            studentas.mediana = (studentas.nd[ndKiekis / 2] + studentas.nd[(ndKiekis / 2) - 1]) / 2.0;
-        else
-            studentas.mediana = studentas.nd[ndKiekis / 2];
-
-        cout << "Iveskite egzamino rezultata:" << endl;
-        cin >> studentas.egzas;
-
-        studentas.galutinisVid = 0.4 * (studentas.ndSuma / ndKiekis) + 0.6 * studentas.egzas;
-        studentas.galutinisMed = 0.4 * studentas.mediana + 0.6 * studentas.egzas;
-
-        stud.push_back(studentas);
-
-        cout << "Ar norite ivesti dar vieno mokinio duomenis? (y/n)" << endl;
-        cin >> pasirinkimas;
-        pasirinkimas = tolower(pasirinkimas);
-        if (pasirinkimas == 'n')
-            break;
-    }
-
+void isvedimas (vector<studentaiStruct> &stud, int maxVardoIlgis, int maxPavardesIlgis) {
     cout << left 
          << setw(maxPavardesIlgis + 2) << "Pavarde" 
          << setw(maxVardoIlgis + 2) << "Vardas" 
@@ -85,6 +47,88 @@ int main() {
              << setw(17) << fixed << setprecision(2) << student.galutinisVid
              << setw(15) << fixed << setprecision(2) << student.galutinisMed << endl;
     }
+}
 
+int main() {
+    vector<studentaiStruct> stud;
+    srand(time(nullptr));
+    int maxVardoIlgis = 6, maxPavardesIlgis = 7; 
+    int meniuPasirinkimas;
+    while (true) {
+        studentaiStruct studentas;
+        studentas.ndSuma = 0;
+
+        cout << "Meniu. Pasirinkite viena is galimu variantu:\n"
+             << "1. Studento duomenis ivesti rankiniu budu.\n"
+             << "2. Ivesti studento varda/pavarde rankiniu budu, o pazymius sugeneruoti atsitiktiniu budu.\n"
+             << "3. Studento varda, pavarde, pazymius sugeneruoti atsitiktiniu budu\n"
+             << "4. Baigti darba, rodyti rezultatus.\n"
+             << "Iveskite pasirinkima: \n";
+        cin >> meniuPasirinkimas;
+
+        if (meniuPasirinkimas == 4)
+            break;
+        if (meniuPasirinkimas == 1){
+
+            vardoIvedimas(studentas);
+
+            maxVardoIlgis = max(maxVardoIlgis, int(studentas.vardas.length()));
+            maxPavardesIlgis = max(maxPavardesIlgis, int(studentas.pavarde.length()));
+
+            char pasirinkimas;
+            int i = 0;
+            while (true) {
+                int ndRezultatas;
+                cout << "Iveskite " << i + 1 << " namu darbo rezultata:" << endl;
+                cin >> ndRezultatas;
+                studentas.nd.push_back(ndRezultatas);
+                studentas.ndSuma += ndRezultatas;
+                cout << "Ar norite ivesti dar viena namu darbo rezultata? (y/n)" << endl;
+                cin >> pasirinkimas;
+                pasirinkimas = tolower(pasirinkimas);
+                if (pasirinkimas == 'n')
+                    break;
+                i++;
+            }
+            cout << "Iveskite egzamino rezultata:" << endl;
+            cin >> studentas.egzas;
+            skaiciavimai(studentas);
+            stud.push_back(studentas);
+
+            cout << "Ar norite ivesti dar vieno mokinio duomenis? (y/n)" << endl;
+            cin >> pasirinkimas;
+            pasirinkimas = tolower(pasirinkimas);
+            if (pasirinkimas == 'n')
+                break;
+        } else if (meniuPasirinkimas == 2) {
+            vardoIvedimas(studentas);
+            maxVardoIlgis = max(maxVardoIlgis, int(studentas.vardas.length()));
+            maxPavardesIlgis = max(maxPavardesIlgis, int(studentas.pavarde.length()));
+            studentas.nd.resize(10);
+            for (int i = 0; i < 10; i++) {
+                studentas.nd[i] = rand() % 10 + 1;
+                studentas.ndSuma += studentas.nd[i];
+            }
+            studentas.egzas = rand() % 10 + 1;
+            skaiciavimai(studentas);
+            stud.push_back(studentas);
+    
+        } else if (meniuPasirinkimas == 3) {
+            studentas.vardas = "Vardas" + to_string(stud.size() + 1);
+            studentas.pavarde = "Pavarde" + to_string(stud.size() + 1);
+            maxVardoIlgis = max(maxVardoIlgis, int(studentas.vardas.length()));
+            maxPavardesIlgis = max(maxPavardesIlgis, int(studentas.pavarde.length()));
+            studentas.nd.resize(10);
+            for (int i = 0; i < 10; i++) {
+                studentas.nd[i] = rand() % 10 + 1;
+                studentas.ndSuma += studentas.nd[i];
+            }
+            studentas.egzas = rand() % 10 + 1;
+            skaiciavimai(studentas);
+            stud.push_back(studentas);
+        }
+    }
+
+    isvedimas(stud, maxVardoIlgis, maxPavardesIlgis);
     return 0;
 }
