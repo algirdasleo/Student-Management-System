fc45cc81a9d5a1d4a9b4cc7e673d0ae05020b930
Plexukas <algirdasleonardas@gmail.com>
2024-03-01 11:44:57 +0200

    prideta kursiokai txt

--- main.cpp
+++ main.cpp
@@ -26,16 +26,18 @@ int main() {
              << "2. Ivesti studento varda/pavarde rankiniu budu, o pazymius sugeneruoti atsitiktiniu budu.\n"
              << "3. Studento varda, pavarde, pazymius sugeneruoti atsitiktiniu budu\n"
              << "4. Nuskaityti studentu duomenis is failo.\n"
-             << "5. Baigti darba, rodyti rezultatus.\n"
+             << "5. Sugeneruoti studentu duomenu faila.\n"
+             << "6. Baigti darba, rodyti rezultatus.\n"
              << "Iveskite pasirinkima: \n";
         cin >> input;
+        cout << endl;
 
-        while (!isNumber(input) || stoi(input) < 1 || stoi(input) > 5) {
-            cout << "Neteisingas pasirinkimas. Iveskite skaiciu nuo 1 iki 5:" << endl;
+        while (!isNumber(input) || stoi(input) < 1 || stoi(input) > 6) {
+            cout << "Neteisingas pasirinkimas. Iveskite skaiciu nuo 1 iki 6:" << endl;
             cin >> input;
         }
         meniuPasirinkimas = stoi(input);
-        if (meniuPasirinkimas == 5)
+        if (meniuPasirinkimas == 6)
             break;
 
         if (meniuPasirinkimas == 1 || meniuPasirinkimas == 2)
@@ -135,6 +137,52 @@ int main() {
             clock_t end2 = clock();
             double laikas2 = double(end2 - start2) / CLOCKS_PER_SEC;
             cout << "Studentu duomenys nuskaityti is failo per " << laikas2 << " sekundziu.\n\n";
+        } else if (meniuPasirinkimas == 5) {
+            cout << "Ar norite nurodyti studentu kieki - 1 \n"
+                 << "Ar norite sugeneruoti 5 failus (1'000 / 10'000 / 100'000 / 1'000'000 / 10'000'000 dydzio) - 2\n"
+                 << "Iveskite pasirinkima (1 arba 2): \n";
+            cin >> input;
+            while (input != "1" && input != "2") {
+                cout << "Neteisingas pasirinkimas. Iveskite 1 arba 2:" << endl;
+                cin >> input;
+            }
+
+            int pasirinkimas = stoi(input);
+            if (pasirinkimas == 1) {
+                cout << "Iveskite studentu kieki (nuo 1 iki 10000000):" << endl;
+                cin >> input;
+                while (!isNumber(input) || stoi(input) < 1 || stoi(input) > 10000000) {
+                    cout << "Neteisingas pasirinkimas. Iveskite skaiciu nuo 1 iki 10'000'000:" << endl;
+                    cin >> input;
+                }
+                int kiekis = stoi(input);
+
+                cout << "Kiek namu darbu pazymiu kiekvienam studentui sugeneruoti? (nuo 1 iki 20):" << endl;
+                cin >> input;
+                while (!isNumber(input) || stoi(input) < 1 || stoi(input) > 20) {
+                    cout << "Neteisingas pasirinkimas. Iveskite skaiciu nuo 1 iki 20:" << endl;
+                    cin >> input;
+                }
+                int pazymiuKiekis = stoi(input);
+
+                generuotiFaila(kiekis, pazymiuKiekis);
+
+            } else if (pasirinkimas == 2) {
+                cout << "Kiek namu darbu pazymiu kiekvienam studentui sugeneruoti? (nuo 1 iki 20):" << endl;
+                cin >> input;
+                while (!isNumber(input) || stoi(input) < 1 || stoi(input) > 20) {
+                    cout << "Neteisingas pasirinkimas. Iveskite skaiciu nuo 1 iki 20:" << endl;
+                    cin >> input;
+                }
+                int pazymiuKiekis = stoi(input);
+                clock_t start3 = clock();
+                generuotiFaila(1000, pazymiuKiekis);
+                generuotiFaila(10000, pazymiuKiekis);
+                generuotiFaila(100000, pazymiuKiekis);
+                generuotiFaila(1000000, pazymiuKiekis);
+                generuotiFaila(10000000, pazymiuKiekis);
+                cout << "Failai sugeneruoti per " << double(clock() - start3) / CLOCKS_PER_SEC << " sekundziu.\n\n";
+            }
         }
 
         if (meniuPasirinkimas > 0 && meniuPasirinkimas < 5) {
