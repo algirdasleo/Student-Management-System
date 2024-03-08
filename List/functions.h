b61f2ea92ccb6a9e1a56d590e9793f3b02ef3c91
Plexukas <algirdasleonardas@gmail.com>
2024-03-08 15:59:35 +0200

    is vector -> list

--- List/functions.h
+++ List/functions.h
@@ -7,15 +7,15 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <list>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
 struct studentaiStruct {
     string vardas, pavarde;
-    vector<int> nd;
+    list<int> nd;
     double ndSuma, egzas, galutinisVid, mediana, galutinisMed;
 };
 
@@ -26,9 +26,9 @@ void vardoIvedimas(studentaiStruct &studentas);
 void numberInputProtection(string &input);
 void charInputProtection(string &input);
 void kaipRusiuoti(char &p1, char &p2);
-void isvedimas(vector<studentaiStruct> &stud, int maxVardoIlgis, int maxPavardesIlgis);
-void isvedimas(vector<studentaiStruct> &stud, int maxVardoIlgis, int maxPavardesIlgis, string pavadinimas);
-void isvedimoSortinimas(vector<studentaiStruct> &stud, char pasirinkimas, char input);
+void isvedimas(list<studentaiStruct> &stud, int maxVardoIlgis, int maxPavardesIlgis);
+void isvedimas(list<studentaiStruct> &stud, int maxVardoIlgis, int maxPavardesIlgis, string pavadinimas);
+void isvedimoSortinimas(list<studentaiStruct> &stud, char pasirinkimas, char input);
 
 void generuotiFaila(int kiekis, int pazymiuKiekis);
 
