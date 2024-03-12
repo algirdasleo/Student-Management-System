9924f2295dab21674e164571a28c362a682af5ee
Plexukas <algirdasleonardas@gmail.com>
2024-03-12 15:38:23 +0200

    pritaikyta naudoti klases

--- main.cpp
+++ main.cpp
@@ -13,15 +13,12 @@ using namespace std;
 
 int main() {
     clock_t start = clock();
-    list<studentaiStruct> stud;
+    list<Studentas> studList;
     srand(time(nullptr));
     int maxVardoIlgis = 6, maxPavardesIlgis = 7;
     string input;
     int meniuPasirinkimas;
     while (true) {
-        studentaiStruct studentas;
-        studentas.ndSuma = 0;
-
         cout << "\nMeniu. Pasirinkite viena is galimu variantu:\n"
              << "1. Studento duomenis ivesti rankiniu budu.\n"
              << "2. Ivesti studento varda/pavarde rankiniu budu, o pazymius sugeneruoti atsitiktiniu budu.\n"
@@ -41,16 +38,17 @@ int main() {
         if (meniuPasirinkimas == 6)
             break;
 
+        Studentas st;
+
         if (meniuPasirinkimas == 1) {
             cout << "1. STUDENTO DUOMENU IVEDIMAS\n";
-            vardoIvedimas(studentas);
+            st.readName();
             int i = 0;
             while (true) {
                 cout << "Iveskite " << i + 1 << " namu darbo rezultata (1 - 10):" << endl;
                 cin >> input;
                 numberInputProtection(input);
-                studentas.nd.push_back(stoi(input));
-                studentas.ndSuma += stoi(input);
+                st.addGrade(stoi(input));
                 cout << "Ar norite ivesti dar viena namu darbo rezultata? (y / n)" << endl;
                 cin >> input;
                 charInputProtection(input);
@@ -61,21 +59,19 @@ int main() {
             cout << "Iveskite egzamino rezultata (1 - 10):" << endl;
             cin >> input;
             numberInputProtection(input);
-            studentas.egzas = stoi(input);
-            skaiciavimai(studentas);
-            stud.push_back(studentas);
+            st.setEgz(stoi(input));
+            st.calculate();
+            studList.push_back(st);
         } else if (meniuPasirinkimas == 2) {
             cout << "2. VARDO, PAVARDES IVEDIMAS. PAZYMIU GENERAVIMAS. \n";
-            vardoIvedimas(studentas);
-            studentas.nd.resize(10);
+            st.readName();
             for (int i = 0; i < 10; i++) {
                 int random_grade = rand() % 10 + 1;
-                studentas.nd.push_back(random_grade);
-                studentas.ndSuma += random_grade;
+                st.addGrade(random_grade);
             }
-            studentas.egzas = rand() % 10 + 1;
-            skaiciavimai(studentas);
-            stud.push_back(studentas);
+            st.setEgz(rand() % 10 + 1);
+            st.calculate();
+            studList.push_back(st);
             cout << "Studento pazymiai sugeneruoti atsitiktinai.\n \n";
         } else if (meniuPasirinkimas == 3) {
             cout << "3. DUOMENU GENERAVIMAS\n";
@@ -88,17 +84,14 @@ int main() {
 
             int kiek = stoi(input);
             for (int i = 0; i < kiek; i++) {
-                studentas.vardas = "Vardas" + to_string(stud.size() + 1);
-                studentas.pavarde = "Pavarde" + to_string(stud.size() + 1);
-                studentas.nd.resize(10);
-                for (int i = 0; i < 10; i++) {
-                    int random_grade = rand() % 10 + 1;
-                    studentas.nd.push_back(random_grade);
-                    studentas.ndSuma += random_grade;
-                }
-                studentas.egzas = rand() % 10 + 1;
-                skaiciavimai(studentas);
-                stud.push_back(studentas);
+                st.setName("Vardas" + to_string(studList.size() + 1));
+                st.setSurname("Pavarde" + to_string(studList.size() + 1));
+                for (int i = 0; i < 10; i++)
+                    st.addGrade(rand() % 10 + 1);
+
+                st.setEgz(rand() % 10 + 1);
+                st.calculate();
+                studList.push_back(st);
             }
             cout << "Studento duomenys sugeneruoti atsitiktinai. \n \n";
         } else if (meniuPasirinkimas == 4) {
@@ -117,46 +110,18 @@ int main() {
                     break;
                 }
             }
-            list<studentaiStruct> failoStud;
-            cout << "Nuskaitomi studentu duomenys is failo...\n";
-            clock_t start2 = clock();
-            string line;
-            getline(in, line);
-            while (getline(in, line)) {
-                istringstream iss(line);
-                studentaiStruct studentas;
-                iss >> studentas.vardas >> studentas.pavarde;
-                studentas.ndSuma = 0;
-                int pazymys;
-                while (iss >> pazymys) {
-                    studentas.nd.push_back(pazymys);
-                    studentas.ndSuma += pazymys;
-                    if (iss.peek() == '\n') break;
-                }
-                if (!studentas.nd.empty()) {
-                    studentas.egzas = studentas.nd.back();
-                    studentas.nd.pop_back();
-                    studentas.ndSuma -= studentas.egzas;
-                    skaiciavimai(studentas);
-                }
-                maxVardoIlgis = max(maxVardoIlgis, int(studentas.vardas.length()));
-                maxPavardesIlgis = max(maxPavardesIlgis, int(studentas.pavarde.length()));
-                failoStud.push_back(move(studentas));
-            }
-            in.close();
-            clock_t end2 = clock();
-            double laikas2 = double(end2 - start2) / CLOCKS_PER_SEC;
-            cout << "Studentu duomenys nuskaityti is failo per " << laikas2 << " sekundziu.\n\n";
+            list<Studentas> failoStud;
+            st.readFromFile(failoStud, input, maxVardoIlgis, maxPavardesIlgis);
 
-            if (!stud.empty()) {
+            if (!studList.empty()) {
                 cout << "Ar norite prideti savo anksciau ivestu ar generuotu studentu duomenis? (y / n)" << endl;
                 cin >> input;
                 charInputProtection(input);
                 if (input == "y") {
-                    for (auto &studentas : stud) {
-                        failoStud.push_back(studentas);
+                    for (auto &st : studList) {
+                        failoStud.push_back(st);
                     }
-                    stud.clear();
+                    studList.clear();
                 }
             }
 
@@ -165,75 +130,24 @@ int main() {
             charInputProtection(input);
             if (input == "y") {
                 clock_t start4 = clock();
-
-                /* 1 STRATEGIJA
-                list<studentaiStruct> islaike, neislaike;
-                for (auto &studentas : failoStud) {
-                    if (studentas.galutinisVid >= 5 && studentas.galutinisMed >= 5)
-                        islaike.push_back(studentas);
-                    else
-                        neislaike.push_back(studentas);
-                }
-                cout << "Studentai islaikyti ir neislaikyti surusiuoti per " << double(clock() - start4) / CLOCKS_PER_SEC << " sekundziu.\n\n";
-                char pasirinkimas, rusiavimas;
-                kaipRusiuoti(pasirinkimas, rusiavimas);
-                isvedimoSortinimas(islaike, pasirinkimas, rusiavimas);
-                isvedimoSortinimas(neislaike, pasirinkimas, rusiavimas);
-                clock_t start5 = clock();
-                isvedimas(islaike, maxVardoIlgis, maxPavardesIlgis, "islaike.txt");
-                isvedimas(neislaike, maxVardoIlgis, maxPavardesIlgis, "neislaike.txt");
-                */
-                /* 2 STRATEGIJA
-                list<studentaiStruct> neislaike;
-                failoStud.sort([](const studentaiStruct& a, const studentaiStruct& b) {
-                    bool aPassed = a.galutinisVid >= 5 && a.galutinisMed >= 5;
-                    bool bPassed = b.galutinisVid >= 5 && b.galutinisMed >= 5;
-                    return !aPassed && bPassed;
-                });
-
-                auto partitionPoint = find_if(failoStud.begin(), failoStud.end(),
-                [](const studentaiStruct& student) {
-                    return student.galutinisVid >= 5 && student.galutinisMed >= 5;
-                });
-
-                neislaike.splice(neislaike.end(), failoStud, failoStud.begin(), partitionPoint);
-
-                cout << "Studentai islaikyti ir neislaikyti surusiuoti per " << double(clock()- start4) / CLOCKS_PER_SEC << " sekundziu.\n\n";
+                list<Studentas> neislaike;
+                st.sortIntoGroups(failoStud, neislaike);
 
                 char pasirinkimas, rusiavimas;
-                kaipRusiuoti(pasirinkimas, rusiavimas);
-                isvedimoSortinimas(failoStud, pasirinkimas, rusiavimas);
-                isvedimoSortinimas(neislaike, pasirinkimas, rusiavimas);
+                howToSort(pasirinkimas, rusiavimas);
+                Studentas::sorting(failoStud, pasirinkimas, rusiavimas);
+                Studentas::sorting(neislaike, pasirinkimas, rusiavimas);
                 clock_t start5 = clock();
-                isvedimas(failoStud, maxVardoIlgis, maxPavardesIlgis, "islaike.txt");
-                isvedimas(neislaike, maxVardoIlgis, maxPavardesIlgis, "neislaike.txt");
-                */
-                // 3 STRATEGIJA (greiciausia)
-                list<studentaiStruct> neislaike;
-                auto it = failoStud.begin();
-                while (it != failoStud.end()) {
-                    if (it->galutinisVid < 5 && it->galutinisMed < 5) {
-                        neislaike.splice(neislaike.end(), failoStud, it++);
-                    } else {
-                        ++it;
-                    }
-                }
-                cout << "Studentai islaikyti ir neislaikyti surusiuoti per " << double(clock() - start4) / CLOCKS_PER_SEC << " sekundziu.\n\n";
-                char pasirinkimas, rusiavimas;
-                kaipRusiuoti(pasirinkimas, rusiavimas);
-                isvedimoSortinimas(failoStud, pasirinkimas, rusiavimas);
-                isvedimoSortinimas(neislaike, pasirinkimas, rusiavimas);
-                clock_t start5 = clock();
-                isvedimas(failoStud, maxVardoIlgis, maxPavardesIlgis, "islaike.txt");
-                isvedimas(neislaike, maxVardoIlgis, maxPavardesIlgis, "neislaike.txt");
+                Studentas::printToFile(failoStud, maxVardoIlgis, maxPavardesIlgis, "islaike.txt");
+                Studentas::printToFile(neislaike, maxVardoIlgis, maxPavardesIlgis, "neislaike.txt");
 
                 cout << "Rezultatai isvesti i failus per " << double(clock() - start5) / CLOCKS_PER_SEC << " sekundziu.\n\n";
             } else {
                 char pasirinkimas, rusiavimas;
-                kaipRusiuoti(pasirinkimas, rusiavimas);
-                isvedimoSortinimas(failoStud, pasirinkimas, rusiavimas);
+                howToSort(pasirinkimas, rusiavimas);
+                Studentas::sorting(failoStud, pasirinkimas, rusiavimas);
                 clock_t start6 = clock();
-                isvedimas(failoStud, maxVardoIlgis, maxPavardesIlgis);
+                Studentas::printToFile(failoStud, maxVardoIlgis, maxPavardesIlgis);
                 cout << "Rezultatai isvesti i faila 'rezultatas.txt' per " << double(clock() - start6) / CLOCKS_PER_SEC << " sekundziu.\n\n";
             }
 
@@ -266,7 +180,7 @@ int main() {
                 }
                 int pazymiuKiekis = stoi(input);
 
-                generuotiFaila(kiekis, pazymiuKiekis);
+                generateFile(kiekis, pazymiuKiekis);
 
             } else if (pasirinkimas == 2) {
                 cout << "Kiek namu darbu pazymiu kiekvienam studentui sugeneruoti? (nuo 1 iki 20):" << endl;
@@ -277,25 +191,24 @@ int main() {
                 }
                 int pazymiuKiekis = stoi(input);
                 clock_t start3 = clock();
-                generuotiFaila(1000, pazymiuKiekis);
-                generuotiFaila(10000, pazymiuKiekis);
-                generuotiFaila(100000, pazymiuKiekis);
-                generuotiFaila(1000000, pazymiuKiekis);
-                generuotiFaila(10000000, pazymiuKiekis);
+                generateFile(1000, pazymiuKiekis);
+                generateFile(10000, pazymiuKiekis);
+                generateFile(100000, pazymiuKiekis);
+                generateFile(1000000, pazymiuKiekis);
+                generateFile(10000000, pazymiuKiekis);
                 cout << "Failai sugeneruoti per " << double(clock() - start3) / CLOCKS_PER_SEC << " sekundziu.\n\n";
             }
         }
 
         if (meniuPasirinkimas > 0 && meniuPasirinkimas < 5) {
-            maxVardoIlgis = max(maxVardoIlgis, int(studentas.vardas.length()));
-            maxPavardesIlgis = max(maxPavardesIlgis, int(studentas.pavarde.length()));
+            string vardas = st.getName();
+            string pavarde = st.getSurname();
+            maxVardoIlgis = max(maxVardoIlgis, int(vardas.length()));
+            maxPavardesIlgis = max(maxPavardesIlgis, int(pavarde.length()));
         }
     }
-
-    clock_t end = clock();
-    double laikas = double(end - start) / CLOCKS_PER_SEC;
-    if (!stud.empty())
-        isvedimas(stud, maxVardoIlgis, maxPavardesIlgis);
-    cout << "Programa uztruko: " << laikas << " sekundziu." << endl;
+    if (!studList.empty())
+        Studentas::printToFile(studList, maxVardoIlgis, maxPavardesIlgis);
+    cout << "Programa uztruko: " << clock() - start << " sekundziu." << endl;
     return 0;
 }
