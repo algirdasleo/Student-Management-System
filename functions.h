7cb1e7ed8afb288f8b2b3720c9758d41accfb3b2
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-04-15 14:19:12 +0300

    IStream overloading + new functions

--- functions.h
+++ functions.h
@@ -2,6 +2,7 @@
 #define FUNCTIONS_H
 
 #include <algorithm>
+#include <chrono>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
@@ -10,7 +11,6 @@
 #include <list>
 #include <sstream>
 #include <string>
-#include <chrono>
 
 class Studentas;  // Forward declaration
 
@@ -32,7 +32,8 @@ public:
     ~StudentasManager();
 
     size_t getStudentListSize();
-    void addStudent(Studentas student);
+    void addStudent(Studentas &student);
+    void generateRandomStudents(int count);
     void readFromFile(std::string fileName);
     void setMaxNameLength(int length);
     void setMaxSurnameLength(int length);
@@ -41,14 +42,18 @@ public:
     void sortIntoGroups(std::list<Studentas> &neislaike);
     void sortStudents(char choice1, char choice2);
     void sortStudents(char choice1, char choice2, std::list<Studentas> &neislaike);
+    void groupAndPrint();
+    void noGroupPrint();
     void printToFile();                      // Print to a default file name Rezultatas.txt
     void printToFile(std::string fileName);  // Print to a custom file name
     void printToFile(std::string fileName, std::list<Studentas> &neislaike);
 };
 
 class Studentas {  // Class for student data
-private:
+protected:
     std::string vardas, pavarde;
+
+private:
     std::list<int> ndPazymiai;
     double egzPazymys, galBalasVid, galBalasMed;
 
@@ -56,16 +61,14 @@ public:
     Studentas();
     ~Studentas();
 
-    friend class StudentasManager;                                                                     // StudentasManager can access private members of Studentas
     friend std::ostream &operator<<(std::ostream &out, const StudentasManager::OutputHelper &helper);  // OutputHelper can access private members of Studentas
-
-    // Rule of Five
     Studentas(const Studentas &stud);
     Studentas &operator=(const Studentas &stud);
     Studentas(Studentas &&stud) noexcept;
     Studentas &operator=(Studentas &&stud) noexcept;
 
     friend std::istream &operator>>(std::istream &in, Studentas &stud);
+    friend class StudentasManager;
 
     void setName(std::string name);
     std::string getName();
@@ -77,6 +80,47 @@ public:
     void calculate();
 };
 
+// Derived classes for different ways of reading student data
+
+class ReadStudentas {
+public:
+    virtual std::istream &read(std::istream &is, StudentasManager &manager, Studentas &student) = 0;
+};
+
+class ManualStudentas : public ReadStudentas {
+public:
+    std::istream &read(std::istream &is, StudentasManager &manager, Studentas &student) override;
+
+private:
+    void readName(std::istream &is, Studentas &student);
+    void getUserGradeInput(std::istream &is, Studentas &student);
+};
+
+class SemiAutoStudentas : public ReadStudentas {
+public:
+    std::istream &read(std::istream &is, StudentasManager &manager, Studentas &student) override;
+
+private:
+    void readName(std::istream &is, Studentas &student);
+    void generateGrades(Studentas &st);
+};
+
+class AutoStudentas : public ReadStudentas {
+public:
+    std::istream &read(std::istream &is, StudentasManager &manager, Studentas &student) override;
+
+private:
+    void enterFileName(std::istream &is, StudentasManager &manager, Studentas &student);
+};
+
+class FileStudentas : public ReadStudentas {
+public:
+    std::istream &read(std::istream &is, StudentasManager &manager, Studentas &student) override;
+
+private:
+    std::string enterFileName(std::istream &is);
+};
+
 void testStudentasClass();  // Testing Rule of Five for Studentas class
 
 void numberInputProtection(std::string &input);
@@ -85,5 +129,10 @@ bool isNumber(std::string &str);
 bool isString(std::string &str);
 void howToSort(char &choice1, char &choice2);
 void generateFile(int range, int homeworkCount);
+int getMenuChoice();
+void getUserGradeInput(Studentas &st);
+void enterFileName(std::ifstream &in, std::string &fileName);
+void rangeStudentGenerator();
+void fiveFileGenerator();
 
 #endif
